707cefdd904a77bd317bacd637b48d5f4d2fab67
ChrissQuartz <69059820+thecrazytech1@users.noreply.github.com>
2022-04-08 09:46:43 +1000

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,145 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Image, Box, Section, Text, Icon, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Override, Menu, StackItem, Stack } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Image display="block" src="https://uploads.quarkly.io/624f70bf5ac830002350fe26/images/logo_2000x20005000.png?v=2022-04-07T23:22:31.120Z" width="50px" />
+				<Link href="#" color="#000000" />
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+					align-items="center"
+					flex-direction="row"
+					position="relative"
+					right="-300px"
+				>
+					<Override
+						slot="link"
+						text-decoration="none"
+						color="--dark"
+						padding="6px 12px"
+						href="/"
+					>
+						Home
+					</Override>
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px 6px 6px 6px" text-align="center" position="static" />
+				</Menu>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+					align-items="center"
+					flex-direction="row"
+				>
+					<Override
+						slot="link"
+						text-decoration="none"
+						color="--dark"
+						padding="6px 12px"
+						href="#about"
+					>
+						About
+					</Override>
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px 6px 6px 6px" text-align="center" position="static" />
+				</Menu>
+			</Box>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+			transition="all 2s ease-in 0s"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Striving for perfection in everything we do. Unparalleled service for everyone.
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section padding="80px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 20px 0" text-align="center">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Button font="--lead" margin="20px">
+				Button
+			</Button>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				hello@company.com
+			</Link>
+		</Section>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"624f70bf5ac830002350fe24"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"624f70bf5ac830002350fe2b",
+				"624f70bf5ac830002350fe2e"
+			]
+		},
+		"624f70bf5ac830002350fe2b": {
+			"id": "624f70bf5ac830002350fe2b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"624f70bf5ac830002350fe2e": {
+			"id": "624f70bf5ac830002350fe2e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
